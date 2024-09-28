76788053b887ab9752b31fcada0064861755ab9f
YeChan Kim <az20058@naver.com>
2024-09-29 01:27:09 +0900

    ⚙Settings: App.jsx 비웠음

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import './App.css'
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/tourism/Home';
-import CourseHome from './pages/courseSelection/CourseHome';
-import Header from './components/header/Header.jsx';
 
 export default function App() {
-
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/CourseHome" element={<CourseHome />} />
-      </Routes>
-    </Router>
+    <>
+    
+    </>
   )
 }
