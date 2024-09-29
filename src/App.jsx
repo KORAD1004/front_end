2c8ec02d0cbce15ec323477b1361426b16e7ed2e
YeChan Kim <az20058@naver.com>
2024-09-30 01:05:14 +0900

    ✨Feat: App.jsx에 라우팅 기능 추가(홈화면, 방사선량 페이지, 소개페이지, 코스선택, 관광홈페이지)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './app.css';
+import Main from './pages/main/main.jsx';
+import Radiation from './pages/radiation/Radiation.jsx';
+import TourismHome from './pages/tourism/Home.jsx';
+import Introduction from './pages/introduction/introduction.jsx';
+import CourseHome from './pages/courseSelection/CourseHome.jsx';
 
 export default function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/radiation' element={<Radiation/>}/>
+        <Route path='/tourism' element={<TourismHome/>}/>
+        <Route path='/introduction' element={<Introduction/>}/>
+        <Route path='/courseSelection' element={<CourseHome/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
