76385f6bdfc2718c5af2930a17598e39f2a067b2
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-10-02 03:58:22 +0900

    🎨 메인 페이지, 소개페이지  디자인 수정

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import CourseHome from './pages/courseSelection/CourseHome.jsx';
 
 export default function App() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path='/' element={<Main/>}/>
-    //     <Route path='/radiation' element={<Radiation/>}/>
-    //     <Route path='/tourism' element={<TourismHome/>}/>
-    //     <Route path='/introduction' element={<Introduction/>}/>
-    //     <Route path='/courseSelection' element={<CourseHome/>}/>
-    //   </Routes>
-    // </BrowserRouter>
-    <Introduction />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/radiation' element={<Radiation/>}/>
+        <Route path='/tourism' element={<TourismHome/>}/>
+        <Route path='/introduction' element={<Introduction/>}/>
+        <Route path='/courseSelection' element={<CourseHome/>}/>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
