d5d96fc5b998e503e0258a3caecdff7e52de28c9
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-10-02 04:23:48 +0900

    ✨ 코드 검색 페이지 추가

---
+++ src/pages/findcode/findcode.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from '../../styles/findcode/findcode.module.css'
+
+function App() {
+    return (
+      <div className={styles["app-container"]}>
+
+      </div>
+    );
+  }
+  
+  export default App;
