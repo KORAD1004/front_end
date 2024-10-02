b29e34129f95d643fe07f056c125a49bddf409bc
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-10-02 19:09:04 +0900

    🎨 코드 찾기 페이지 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,20 @@ import TourismHome from './pages/tourism/Home.jsx';
 import Introduction from './pages/introduction/introduction.jsx';
 import CourseHome from './pages/courseSelection/CourseHome.jsx';
 import Header from './components/header/Header.jsx';
+import FindCode from './pages/findcode/findcode.jsx'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Main/>}/>
-        <Route path='/radiation' element={<Radiation/>}/>
-        <Route path='/tourism' element={<TourismHome/>}/>
-        <Route path='/introduction' element={<Introduction/>}/>
-        <Route path='/courseSelection' element={<CourseHome/>}/>
-      </Routes>
-    </BrowserRouter>
-
+    // <BrowserRouter>
+    //   <Header/>
+    //   <Routes>
+    //     <Route path='/' element={<Main/>}/>
+    //     <Route path='/radiation' element={<Radiation/>}/>
+    //     <Route path='/tourism' element={<TourismHome/>}/>
+    //     <Route path='/introduction' element={<Introduction/>}/>
+    //     <Route path='/courseSelection' element={<CourseHome/>}/>
+    //   </Routes>
+    // </BrowserRouter>
+    <FindCode />
   )
 }
--- src/pages/findcode/findcode.jsx
+++ src/pages/findcode/findcode.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import back from '../../assets/images/findcode/background.svg';
+import midback from '../../assets/images/findcode/midback.svg';
+import styles from '../../styles/findcode/findcode.module.css';
 
-import styles from '../../styles/findcode/findcode.module.css'
-
-function App() {
+function App() { 
+  
     return (
       <div className={styles["app-container"]}>
+        <div className={styles["top-container"]}>
+        <img src={back} className={styles["back"]} alt="back" />
+        <p className={styles["top-comment"]}>
+          나의 여행일정
+        </p>
+        </div>
+
+        <div className={styles["middle-container"]}>
+          <div className={styles["comment-container"]}>
+        <p className={styles["middle-comment1"]}>
+          설정 코스 검색 및 입력하기
+        </p>
+        <p className={styles["middle-comment2"]}>
+          내가 만든 코스
+        </p>
+        </div>
+        <img src={midback} className={styles["midback"]} alt="mid back" />
+        <button className={styles['midbutton']}> 코스 추가하기</button>
+        </div>
 
       </div>
     );
