b444c9f6c59180716da533e702da8c8623ae1bf9
YeChan Kim <az20058@naver.com>
2024-10-03 19:02:13 +0900

    ✨Feat: 방폐물페이지에서 특정 부분까지 스크롤이 내려가면 버튼이 생기고, 버튼을 누르면 화면 위로 올라가는 버튼 추가

--- src/pages/radWaste/RadWaste.jsx
+++ src/pages/radWaste/RadWaste.jsx
@@ -1,18 +1,26 @@
 import styles from '../../styles/radWaste/radWaste.module.css';
 import { Suspense, lazy } from 'react';
 import Loading from '../../components/loading/Loading';
+import { useRef } from 'react';
 const HeaderSection = lazy(() => import('../../components/radWaste/HeaderSection'));
 const ContainerSection = lazy(() => import('../../components/radWaste/ContainerSection'));
 const InfoSection = lazy(() => import('../../components/radWaste/InfoSection'));
 
 export default function RadWaste() {
+    const scrollRef = useRef(null);
+    
+    function onClick() {
+        if (scrollRef.current) {
+            scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
 
     return (
         <Suspense fallback={<Loading/>}>
-            <div className={styles.pageWrapper}>
+            <div ref={scrollRef} className={styles.pageWrapper}>
                 <HeaderSection/>
                 <ContainerSection/>
-                <InfoSection/>
+                <InfoSection onClick={onClick}/>
             </div>
         </Suspense>
     )
