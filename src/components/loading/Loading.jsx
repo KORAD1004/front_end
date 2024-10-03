1b1923aa7d66e8370598979a9ab179ef59f242e5
YeChan Kim <az20058@naver.com>
2024-10-03 16:01:50 +0900

    🎨Design: Loading.jsx ui 변경, 방사선량 페이지에 Loading ui 추가

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -2,7 +2,7 @@ import { ProgressBar } from "react-loader-spinner";
 
 export default function Loading() {
     return (
-        <div style={{display:"flex", justifyContent:"center", height:"100vh", alignItems:"center"}}>
+        <div style={{display:"flex",width:"100vw", justifyContent:"center", height:"100vh", alignItems:"center"}}>
             <ProgressBar
             visible={true}
             height="80"
--- src/pages/radiation/Radiation.jsx
+++ src/pages/radiation/Radiation.jsx
@@ -1,12 +1,16 @@
-import RadiationHeader from '../../components/radiation/RadiationHeader';
-import RadiationMap from '../../components/radiation/RadiationMap';
+import { Suspense, lazy } from 'react';
 import styles from '../../styles/radiation/radiation.module.css';
+import Loading from '../../components/loading/Loading';
+const RadiationHeader = lazy(() => import('../../components/radiation/RadiationHeader'));
+const RadiationMap = lazy(() => import('../../components/radiation/RadiationMap'));
 
 export default function Radiation() {
     return (
-        <div className={styles.pageWrapper}>
-            <RadiationHeader/>
-            <RadiationMap/>
-        </div>
+        <Suspense fallback={<Loading/>}>
+            <div className={styles.pageWrapper}>
+                <RadiationHeader/>
+                <RadiationMap/>
+            </div>
+        </Suspense>
     )
 }
