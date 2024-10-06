68eccddcba8f5aef2cf8f8ff337d2f90aa7c8edd
sungyeon <tjddus0731@naver.com>
2024-10-06 21:48:59 +0900

    🎨Design:courseView Section02 이미지 대신 아이콘 사용

--- src/components/courseView/Section02.jsx
+++ src/components/courseView/Section02.jsx
@@ -1,38 +1,25 @@
 import React from "react";
 import styles from '../../styles/courseView/section02.module.css';
 import CourseMap from "./CourseMap";
-import Days from '../../assets/images/courseView/Days.png';
-import Number from '../../assets/images/courseView/Number.png';
-import People from '../../assets/images/courseView/People.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt, faClock, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Section02 = () => {
     return (
         <div className={styles.allContainer}>
             <div className={styles.scheduleContainer}>
-                <img 
-                        src={Days}
-                        alt="course Days" 
-                        className={styles.scheduleImage}
-                />
-                <p className={styles.scheduleText}>
-                    24.10.11 ~ 24.10.14
-                </p>
-                <img 
-                        src={Number}
-                        alt="course Number" 
-                        className={styles.scheduleImage}
-                />
-                <p className={styles.scheduleText}>
-                    2일차
-                </p>
-                <img 
-                        src={People}
-                        alt="course People" 
-                        className={styles.scheduleImage}
-                />
-                <p className={styles.scheduleText}>
-                    4인
-                </p>
+            <div className={styles.scheduleItem}>
+                    <FontAwesomeIcon icon={faCalendarAlt} className={styles.icon} />
+                    <p className={styles.scheduleText}>24.10.11 ~ 24.10.14</p>
+                </div>
+                <div className={styles.scheduleItem}>
+                    <FontAwesomeIcon icon={faClock} className={styles.icon} />
+                    <p className={styles.scheduleText}>2일차</p>
+                </div>
+                <div className={styles.scheduleItem}>
+                    <FontAwesomeIcon icon={faUser} className={styles.icon} />
+                    <p className={styles.scheduleText}>4인</p>
+                </div>
             </div>
             <div className={styles.underline}></div>
             <CourseMap />
