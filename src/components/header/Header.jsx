22c128ec5841c1b32a331bb51054b176cac1302e
YeChan Kim <az20058@naver.com>
2024-10-07 11:03:17 +0900

    Route: 네비게이션에 방폐물 페이지 네비게이션 추가

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
       </div>
         <div className={`${styles.menu} ${menuOpen ? styles.open : ''}`}>
           <ul>
-            <li onClick={()=>navigate('/#')}>방폐물이란?</li>
+            <li onClick={()=>navigate('/radWaste')}>방폐물이란?</li>
             <li onClick={()=>navigate('/radiation')}>원자력안전도</li>
             <li onClick={()=>navigate('/tourism')}>경주관광지</li>
             <li onClick={()=>navigate('/myTrip')}>나의여행일정</li>
