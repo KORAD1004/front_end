450facab8a432bf86ef65f019b682e7c6fdd3e21
sungyeon <tjddus0731@naver.com>
2024-10-07 23:48:51 +0900

    🐛Fix: 렌더링 2번 오류 수정

--- src/components/myTrip/Section03.jsx
+++ src/components/myTrip/Section03.jsx
@@ -1,13 +1,13 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import styles from '../../styles/myTrip/section03.module.css';
 
 const Section03 = ({ rows = [] }) => {
     const navigate = useNavigate();
-// 예시: 의존성 배열을 빈 배열([])로 설정하여 마운트될 때 한 번만 실행
-useEffect(() => {
-    console.log("Section03 렌더링");
-}, []);  // 빈 배열을 사용하여 컴포넌트가 처음 마운트될 때만 실행
+    // 예시: 의존성 배열을 빈 배열([])로 설정하여 마운트될 때 한 번만 실행
+    useEffect(() => {
+        console.log("Section03 렌더링");
+    }, []);  // 빈 배열을 사용하여 컴포넌트가 처음 마운트될 때만 실행
 
 
     return (
@@ -28,22 +28,21 @@ useEffect(() => {
                         </div>
                     ))
                 ) : (
-                    null
+                    <div className={styles.buttonContainer}>
+                        <button
+                            className={styles.backButton}
+                            onClick={() => navigate(-1)}
+                        >
+                            취소
+                        </button>
+                        <button
+                            className={styles.saveButton}
+                            onClick={() => navigate('/courseView')}
+                        >
+                            저장
+                        </button>
+                    </div>
                 )}
-            </div>
-            <div className={styles.buttonContainer}>
-                <button
-                    className={styles.backButton}
-                    onClick={() => navigate(-1)}
-                >
-                    취소
-                </button>
-                <button
-                    className={styles.saveButton}
-                    onClick={() => navigate('/courseView')}
-                >
-                    저장
-                </button>
             </div>
         </div>
     );
