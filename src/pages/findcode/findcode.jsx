1e7dc87fa8c53370308f2893ae96b4f2f1f6cf7b
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-10-10 04:05:52 +0900

    🎨 나의 여행일정 멘트 및 사진 밝기 수정

--- src/pages/findcode/findcode.jsx
+++ src/pages/findcode/findcode.jsx
@@ -80,7 +80,7 @@ function App() {
                   <div className={styles["travel-com"]} onClick={handleImageClick}> 
                     <p className={styles["travel-name"]}>{travelDetails.travelName}</p> 
                     <p className={styles["travel-comment"]}>
-                      | {`${travelDetails.startDate} ~ ${travelDetails.endDate} 중 ${travelDetails.days}`}
+                      | {`${travelDetails.startDate} ~ ${travelDetails.endDate} 中 ${travelDetails.days}일차`}
                     </p>
                   </div>
                   <div className={styles["image-list"]}>
