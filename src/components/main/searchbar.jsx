e1ae218ddc377f76e7aa6081c2b880bbc70ec076
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-10-10 04:02:06 +0900

    🎨 메인 페이지 종이비행기 추가, vw 설정

--- src/components/main/searchbar.jsx
+++ src/components/main/searchbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../../styles/main/main.module.css';
+import paper from '../../assets/images/main/paper.svg'; // 이미지 경로
 
 function SearchBar() {
   const navigate = useNavigate(); 
@@ -8,7 +9,8 @@ function SearchBar() {
   return (
     <div className={styles['search-bar-container']}>
       <div className={styles['search-input']}>
-        경주 방폐물처리장을 감싸는 가볼만한 장소 추천
+        <span>경주 방폐물처리장을 감싸는 가볼만한 장소 추천</span>
+        <img src={paper} alt="Paper Icon" className={styles['paper-icon']} />
       </div>
       <button className={styles['search-button']} onClick={() => navigate('/tourism')}>
         바로가기
