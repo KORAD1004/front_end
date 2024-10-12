8079be366cedb2f360e66388e09afbd622eb6b07
MeongW <easymiracle@skuniv.ac.kr>
2024-10-12 18:17:04 +0900

    ♻️Feat: 이미지 슬라이드 뷰에 스와이퍼 적용

--- src/components/tourism/CourseSelect.jsx
+++ src/components/tourism/CourseSelect.jsx
@@ -5,6 +5,10 @@ import id3 from '../../assets/images/tourism/id3.png';
 import styles from '../../styles/tourism/courseSelect.module.css';
 import ImageLazy from '../imgLazy/ImageLazy';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import '../../styles/components/swiper.css';
+
 const images = [
   { id: 1, src: id1, alt: 'Image 1' },
   { id: 2, src: id2, alt: 'Image 2' },
@@ -15,26 +19,22 @@ const CourseSelect = ({ onImageSelect }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const imageRefs = useRef([]);
   const galleryRef = useRef(null);
+  const swiperRef = useRef(null);
 
-  const handleImageClick = (index) => {
-    setCurrentIndex(index);
-    const clickedImage = imageRefs.current[index];
-    const galleryElement = galleryRef.current;
+  const handleSlideChange = (swiper) => {
+    setCurrentIndex(swiper.realIndex);
 
-    if (clickedImage && galleryElement) {
-      const offsetLeft = clickedImage.offsetLeft - galleryElement.offsetWidth / 2 + clickedImage.offsetWidth / 2;
-      galleryElement.scrollTo({
-        left: offsetLeft,
-        behavior: 'smooth',
-      });
-    }
-
-    // 선택된 이미지의 ID를 상위 컴포넌트로 전달
     if (onImageSelect) {
-      onImageSelect(images[index].id); // props로 전달받은 함수 실행
+      onImageSelect(images[swiper.realIndex].id); // props로 전달받은 함수 실행
+      
     } else {
       console.warn("onImageSelect is not provided");
     }
+  };
+
+  const handleImageClick = (swiper, index) => {
+    swiper.slideTo(swiper.clickedIndex);
+    setCurrentIndex(index);
   };
 
   return (
@@ -45,23 +45,37 @@ const CourseSelect = ({ onImageSelect }) => {
             <div
               key={index}
               className={`${styles.dot} ${currentIndex === index ? styles.active : ''}`}
-              onClick={() => handleImageClick(index)}
             />
           ))}
         </div>
       </div>
 
       <div className={styles.imageGallery} ref={galleryRef}>
-        {images.map((image, index) => (
-          <ImageLazy
-            key={image.id}
-            refer={(el) => (imageRefs.current[index] = el)}
-            src={image.src}
-            alt={image.alt}
-            className={`${styles.galleryImage} ${currentIndex === index ? styles.active : ''}`}
-            onClick={() => handleImageClick(index)}
-          />
-        ))}
+        <Swiper
+          ref={swiperRef}
+          spaceBetween={10}
+          autoHeight={true}
+          slidesPerView={'auto'}
+          centeredSlides={true}
+          onSlideChange={handleSlideChange}
+          slidesPerGroup={1}
+          initialSlide={images.length - 2}
+        >
+          {images.map((image, index) => (
+            <SwiperSlide
+              key={image.id}
+              onClick={() => handleImageClick(swiperRef.current.swiper, index)}
+            >
+              <ImageLazy
+                key={image.id}
+                refer={(el) => (imageRefs.current[index] = el)}
+                src={image.src}
+                alt={image.alt}
+                className={`${styles.galleryImage} ${currentIndex === index ? styles.active : ''}`}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
