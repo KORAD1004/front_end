a91ab522ae2f6f179713a499dd5be6ac1deedd85
sungyeon <tjddus0731@naver.com>
2024-10-14 01:20:25 +0900

    🎨+✨: 그림자 ui추가 및 나의 여행일정 이전날짜 막기

--- src/components/myTrip/Section02.jsx
+++ src/components/myTrip/Section02.jsx
@@ -136,10 +136,10 @@ const Section02 = ({ rows, setRows, location, setLocation }) => {
                                 <div className={styles.memoContainer}>
                                     <input
                                         type="text"
-                                        placeholder="메모를 작성해 주세요. (16자 이내)"
+                                        placeholder="메모를 작성해 주세요. (12자 이내)"
                                         className={styles.memoTextBox}
                                         value={row.memo}
-                                        maxLength={16}
+                                        maxLength={12}
                                         onChange={(e) => onMemoChange(row.id, e.target.value)}
                                     />
                                 </div>
