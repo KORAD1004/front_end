d661947d9d7984205393e5e8836e7d7ade323b4f
YeChan Kim <az20058@naver.com>
2024-10-16 18:01:29 +0900

    🎨Design: 방사선 페이지 로딩 UI 추가 및 lazy loading으로 변경

--- src/pages/radiation/Radiation.jsx
+++ src/pages/radiation/Radiation.jsx
@@ -1,12 +1,16 @@
-import RadiationHeader from '../../components/radiation/RadiationHeader.jsx';
-import RadiationMap from '../../components/radiation/RadiationMap.jsx';
+import { Suspense, lazy } from 'react';
+const RadiationHeader = lazy(() => import('../../components/radiation/RadiationHeader.jsx'));
+const RadiationMap = lazy(() => import('../../components/radiation/RadiationMap.jsx'));
 import styles from '../../styles/radiation/radiation.module.css';
+import Loading from '../../components/loading/Loading.jsx';
 
 export default function Radiation() {
     return (
-        <div className={styles.pageWrapper}>
-            <RadiationHeader/>
-            <RadiationMap/>
-        </div>
+        <Suspense fallback={<Loading time={0.5}/>}>
+            <div className={styles.pageWrapper}>
+                <RadiationHeader/>
+                <RadiationMap/>
+            </div>
+        </Suspense>
     )
 }
