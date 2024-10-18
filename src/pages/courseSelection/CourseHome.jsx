27f8469724628ee3d60e06e22c9efcd62d75b117
YeChan Kim <az20058@naver.com>
2024-10-18 19:01:45 +0900

    🚑HOTFIX: 코스 추천 확인 페이지 카드 요소 height 픽셀 고정

--- src/pages/courseSelection/CourseHome.jsx
+++ src/pages/courseSelection/CourseHome.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Section01 from '../../components/courseSelection/Section01.jsx';
 import Section02 from '../../components/courseSelection/Section02.jsx';
 import Section03 from '../../components/courseSelection/Section03.jsx';
