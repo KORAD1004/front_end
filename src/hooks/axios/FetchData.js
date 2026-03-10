4b216fb05966b9621f570ea2536da7d0c148322c
YeChan Kim <az20058@naver.com>
2026-03-10 23:26:30 +0900

    ♻️ Refactor: Suspense 제거

--- src/hooks/axios/FetchData.js
+++ src/hooks/axios/FetchData.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+import radiationMock from "../../assets/data/radiation.json";
+import avgRadMock from "../../assets/data/avgRad.json";
+
+const USE_MOCK = true;
 
 export const fetchRecentRadiation = async () => {
+    if (USE_MOCK) return radiationMock;
     const { data } = await axios.get(
         `${import.meta.env.VITE_SERVER_URL}/api/scrap/radiation/recent/avg`
       );
@@ -8,6 +13,7 @@ export const fetchRecentRadiation = async () => {
 }
 
 export const fetchAvgRadiation = async () => {
+    if (USE_MOCK) return avgRadMock;
     const { data } = await axios.get(
       `${import.meta.env.VITE_SERVER_URL}/api/scrap/radiation/average`
     );
